1c5e1ba74eaa8c621c07c59cdb8e478c7414c8f7
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-02-12 17:15:49 -0300

    test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,4 +3,21 @@
 import unittest
 from models import models.BaseModel
 
+class TestBaseModel(unittest.TestCase):
+    def id_string():
+    def test_more_parameters(self):
 
+    def test_created_at():
+    def noformat_in_created_at():
+
+    def test_update_at():
+    def noformat_updated_at():
+
+    def test_str():
+
+    def test_save():
+
+    def test_to_dict():
+
+    
+    
