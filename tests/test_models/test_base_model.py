376815d0844b7095fd47ec26a4261acfba28716e
mi-kei-la <micaela.percovich@gmail.com>
2021-02-13 14:30:23 +0000

    upload new tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,32 +1,48 @@
 #!/usr/bin/env python3
 """This module contains all unittests for the BaseModel class."""
-import unittest
 from models.base_model import BaseModel
+import unittest
+from datetime import datetime
+
 
 class TestBaseModel(unittest.TestCase):
-    def test_id_string(self):
-        to_test = BaseModel()
-        self.assertIsInstance(to_test.id, str)
-
-    def test_more_parameters(self):
-        to_test = BaseModel()
-
-
-    def test_created_at(self):
-        to_test = BaseModel()
-        self.assertEqual(type)
-    def test_noformat_in_created_at(self):
-        to_test = BaseModel()
-    def test_update_at(self):
-        to_test = BaseModel()
-    def test_noformat_updated_at(self):
-        to_test = BaseModel()
-
-    def test_str(self):
+    """class for creation of tests for base_model module
+    """
+    def test_attributes(self):
+        """ Test the attributes in different cases
+        """
+        inst_1 = BaseModel()
+        # test if id is string
+        self.assertIsInstance(inst_1.id, str)
+        inst_2 = BaseModel()
+        # test different id
+        self.assertNotEqual(inst_1.id, inst_2.id)
+        # test if created_at is datetime
+        self.assertIsInstance(inst_1.created_at, datetime)
+        # test if update_at is datetime
+        self.assertIsInstance(inst_1.updated_at, datetime)
 
     def test_save(self):
+        """ Testing the save method
+        """
+        inst_1 = BaseModel()
+        # test the updated_at before and after save()
+        first_updated_at = inst_1.updated_at
+        inst_1.save()
+        second_updated_at = inst_1.updated_at
+        self.assertNotEqual(first_updated_at, second_updated_at)
+        # test the type of updated_at after save()
+        self.assertIsInstance(inst_1.updated_at, datetime)
 
     def test_to_dict(self):
-
-    
-    
+        """ Test to_dict method.
+        """
+        inst_1 = BaseModel()
+        dict_1 = inst_1.to_dict()
+        # Test type dict
+        self.assertIsInstance(dict_1, dict)
+        inst_1.save()
+        # Test type
+        dict_2 = inst_1.to_dict()
+        # Test if dictionary updates automatically
+        self.assertNotEqual(dict_1["updated_at"], dict_2["updated_at"])
